c37862dcbaec2ca464b50be7ce1762ff5d91e364
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-06-19 20:50:23 -0700

    switch from _s php loop to Timber loop in single.php

--- single.php
+++ single.php
@@ -14,20 +14,42 @@ get_header();
 		<main id="main" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+// while ( have_posts() ) :
+//     the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+//     get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+//     the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+//     // If comments are open or we have at least one comment, load up the comment template.
+//     if ( comments_open() || get_comments_number() ) :
+//         comments_template();
+//     endif;
 
-		endwhile; // End of the loop.
-		?>
+// endwhile; // End of the loop.
+?>
+
+		<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::context();
+$timber_post = Timber::query_post();
+$context['post'] = $timber_post;
+
+if (post_password_required($timber_post->ID)) {
+    Timber::render('single-password.twig', $context);
+} else {
+    Timber::render(array('single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single.twig'), $context);
+}
+?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
