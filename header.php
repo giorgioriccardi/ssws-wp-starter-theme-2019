e5c672ec876fc345419334984ca0a0932d3fa197
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-06-20 10:50:45 -0700

    switch from _s php code to Timber code in header.php

--- header.php
+++ header.php
@@ -10,50 +10,66 @@
  */
 
 ?>
-<!doctype html>
-<html <?php language_attributes();?>>
+<!-- <!doctype html>
+<html <?php //language_attributes();?>>
 <head>
-	<meta charset="<?php bloginfo('charset');?>">
+	<meta charset="<?php //bloginfo('charset');?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head();?>
+	<?php //wp_head();?>
 </head>
 
-<body <?php body_class();?>>
-<?php wp_body_open();?>
-<div id="page" class="site">
+<body <?php body_class();?>> -->
+<?php //wp_body_open();?>
+<!-- <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'ssws-2019');?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+		<div class="site-branding"> -->
 			<?php
-the_custom_logo();
-if (is_front_page() && is_home()):
+// the_custom_logo();
+// if (is_front_page() && is_home()):
 ?>
-				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name');?></a></h1>
+				<!-- <h1 class="site-title"><a href="<?php //echo esc_url(home_url('/')); ?>" rel="home"><?php //bloginfo('name');?></a></h1> -->
 				<?php
-else:
+// else:
 ?>
-				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name');?></a></p>
+				<!-- <p class="site-title"><a href="<?php //echo esc_url(home_url('/')); ?>" rel="home"><?php //bloginfo('name');?></a></p> -->
 				<?php
-endif;
-$ssws_2019_description = get_bloginfo('description', 'display');
-if ($ssws_2019_description || is_customize_preview()):
+// endif;
+// $ssws_2019_description = get_bloginfo('description', 'display');
+// if ($ssws_2019_description || is_customize_preview()):
 ?>
-				<p class="site-description"><?php echo $ssws_2019_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif;?>
-		</div><!-- .site-branding -->
+				<!-- <p class="site-description"><?php //echo $ssws_2019_description; /* WPCS: xss ok. */ ?></p> -->
+			<?php //endif;?>
+		<!--</div>--><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'ssws-2019');?></button>
+		<!-- <nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php //esc_html_e('Primary Menu', 'ssws-2019');?></button> -->
 			<?php
-wp_nav_menu(array(
-    'theme_location' => 'menu-1',
-    'menu_id' => 'primary-menu',
-));
+// wp_nav_menu(array(
+//     'theme_location' => 'menu-1',
+//     'menu_id' => 'primary-menu',
+// ));
 ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		<!--</nav>--><!-- #site-navigation -->
+	<!--</header>--><!-- #masthead -->
 
-	<div id="content" class="site-content">
+	<!-- <div id="content" class="site-content"> -->
+
+<?php
+/**
+ * Third party plugins that hijack the theme will call wp_head() to get the header template.
+ * We use this to start our output buffer and render into the view/page-plugin.twig template in footer.php
+ *
+ * If you're not using a plugin that requries this behavior (ones that do include Events Calendar Pro and
+ * WooCommerce) you can delete this file and footer.php
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+echo '<pre>GRC this is a Timber part8</pre>';
+$GLOBALS['timberContext'] = Timber::context();
+ob_start();
--- page.php
+++ page.php
@@ -19,18 +19,48 @@ get_header();
 		<main id="main" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+// while ( have_posts() ) :
+//     the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+//     get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+//     // If comments are open or we have at least one comment, load up the comment template.
+//     if ( comments_open() || get_comments_number() ) :
+//         comments_template();
+//     endif;
 
-		endwhile; // End of the loop.
-		?>
+// endwhile; // End of the loop.
+?>
+
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * To generate specific templates for your pages you can use:
+ * /mytheme/views/page-mypage.twig
+ * (which will still route through this PHP file)
+ * OR
+ * /mytheme/page-mypage.php
+ * (in which case you'll want to duplicate this file and save to the above path)
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::context();
+
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render(array('page-' . $timber_post->post_name . '.twig', 'page.twig'), $context);
+?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
